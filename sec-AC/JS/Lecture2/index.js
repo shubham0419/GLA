4a37f0a90d1fdb3116b920f85f3d76fc15b153d8
Your_username <YOUR_EMAIL>
2024-09-19 11:19:49 +0530

    dobe

---
+++ sec-AC/JS/Lecture2/index.js
@@ -0,0 +1,26 @@
+// console.log(Math.round(Math.random()*100));
+// console.log(Math.floor(19.9999999999));  // 19
+// console.log(Math.ceil(19.0001));  // 20
+
+// Math.round((Math.random()*(max-min))+min)
+// console.log(Math.round((Math.random()*(25-15))+15));
+// console.log(Math.max(12,12,34,5,6,67,88,66))
+// console.log(Math.pow(3,5))
+
+let str = "this is a string";
+// console.log(str.substr(4,3));
+console.log(str.slice(5,7));
+
+console.log(str.includes("is a"));
+
+const date = "18-09-2024";
+
+// console.log(date.replace("-","/"));
+// console.log(date.replaceAll("-","/"));
+
+// console.log(str.indexOf("s"));
+// console.log(str.lastIndexOf("s"));
+
+const arr = [1,2 ,3,"hiii", true,["2d","array"],null,undefined]
+console.log(arr[5][1]);
+
