f58a40c618aa0c920f2a3be7d59b60014e95a487
Your_username <YOUR_EMAIL>
2024-09-25 09:50:08 +0530

    done

--- Sec-N/JS/Lecture3/index.js
+++ Sec-N/JS/Lecture3/index.js
@@ -24,5 +24,47 @@ console.log(arr);
 // console.log(arr);
 
 // [1,2,3,4,5,6,7]
-arr.splice(4,2,20,22,24);
-console.log(arr);
+// arr.splice(4,2,20,22,24);
+// console.log(arr);
+
+// Non-Destructive function
+// [1,2,3,4,5,6,7]
+// const isPresent = arr.includes(7)
+// console.log(isPresent);
+
+// const slicedArr = arr.slice(3,5);
+// console.log(slicedArr);
+
+// const index = arr.indexOf(6);
+// console.log(index);
+
+// const lastIndex = arr.lastIndexOf(5);
+// console.log(lastIndex);
+
+// // [1,2,3,4,5,6,7]
+// const str = arr.join(" ,");
+// console.log(str);
+
+// const newArr = ["this","is","another","array"];
+// const combinedArr = arr.concat(["this","is","concated"],newArr);
+// console.log(combinedArr);
+
+// // reverse and sort - destructive method
+// arr.reverse();
+// console.log(arr);
+
+// const toSort = [1,2,3,4,5,6,7,8,9,10,11,12];
+// toSort.sort();
+// console.log(toSort);
+
+// Loop --------------------------------
+// [1,2,3,4,5,6,7]
+
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(arr[i]);
+// }
+
+for(let item of arr){
+  console.log(item);
+}
+
---
+++ Sec-S/JS/Lecture4/index.js
@@ -0,0 +1,63 @@
+
+const obj = {
+  name:"kuchbhi",
+  age:24,
+  studing:false,
+  arr:[1,2,3,4,5],
+}
+// // console.log([1,2,3,4,5,6,7,8]);
+// console.log(obj);
+// console.log(obj.arr);
+// console.log(obj["arr"]);
+// console.log(obj.arr[2]);
+
+// // obj.college = "GLA";
+// obj["college"] = "GLA"
+// console.log(obj);
+
+// obj.name = "this is changed";
+// console.log(obj);
+
+// -------------------- Loops -------------------
+let arr = [1,2,3,4,5,6,7,8,9];
+// let i = 0;
+
+// while(i<arr.length){
+//   console.log(arr[i]);
+//   i++;
+// }
+// [1,2,3,4,5,6,7,8,9]
+// for(let i=0;i<arr.length;i++){
+//   console.log(arr[i]);
+// }
+// [1,2,3,4,5,6,7,8,9]
+// for (const item of arr) {
+//   console.log(item);
+// }
+// for (const item in arr) {
+//   console.log(item,"->",arr[item]);
+// }
+
+// {
+//   name:"kuchbhi",
+//   age:24,
+//   studing:false,
+//   arr:[1,2,3,4,5],
+// }
+
+for(let key in obj){
+  console.log(key,"->",obj[key]);
+}
+
+const complexObj = {
+  membership:{
+    name:"Mega",
+    price:20000,
+    devices :[
+      {
+        deviceName:"Iphone 12",
+        ipAddress:"122.681.32.4"
+      }
+    ]
+  }
+}
