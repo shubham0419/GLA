db0f560187801e26dad3a98d98e35bf722021b63
Your_username <YOUR_EMAIL>
2024-09-25 15:38:07 +0530

    logic

--- sec-AC/JS/Lecture4/index.js
+++ sec-AC/JS/Lecture4/index.js
@@ -1,6 +1,4 @@
-
 let a = 10;
-
 // if(a=="10"){
 //   console.log(true);
 // }
@@ -28,3 +26,44 @@ if((a=="10" || a==="10") && (a==100/10 && a==1000/10)){
 }else{
   console.log(false);
 }
+
+// ternary operatoer 
+
+a==="10" ? console.log(true) : console.log(false);
+
+a==10?a==="10"?console.log(true):console.log("type not matched")
+:console.log(false);
+
+if(a==10){
+  if(a==="10"){
+    console.log(true);
+  }else{
+    console.log('type not matched');
+  }
+}else{
+  console.log(false);
+}
+
+const ab = prompt("write a paragraph");
+// console.log(userPara);
+
+// Q check a number is +ve or -ve or zero or string
+(ab<0 || ab>0 || ab==0)?ab==0?console.log("zero"):ab<0?console.log('-Ve')
+:console.log("+ve"):console.log("string");
+
+const arr = [3,"4",5,"6",7,"8",9,10,11,"12"];
+const random = Math.round(Math.random()*(12-3))+3;
+// console.log(typeof("hii"));
+// Q generate a random number number b/w 3 to 12 and check 
+// what is type of that number in array      
+let index = -1;
+for(let i=0;i<arr.length;i++){
+  if(random==arr[i]){
+    index=i;
+    break;
+  }
+}
+console.log(random,typeof(arr[index]));
+
+a==10 && console.log(true);
+if(a==10) console.log(true);
