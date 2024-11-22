ad06daab4503cc4a144a9cc7dcfb921338b1f2da
shubham0419 <shubhamsc704707@gmail.com>
2024-11-22 13:29:43 +0530

    done

--- Sec-AF-AG/React/react-first-app/src/App.jsx
+++ Sec-AF-AG/React/react-first-app/src/App.jsx
@@ -1,19 +1,37 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './components/Navbar'
+import axios from "axios"
 
 function App() {
 
   const name = "Shubham Choudhary"
 
+  let [count,setCount] = useState(0);
+
+  const getUsers = async ()=>{
+    let data = await axios.get("https://api.github.com/users/shubham0419");
+    console.log(data.data);
+  }
+
+  useEffect(()=>{
+    console.log("useEffect is called");
+    // setCount(count+1); // never call setter of dependency getter
+    getUsers();
+  },[count]);
+
   return (
     <div>
       {/* {Navbar()} */}
       {/* <Navbar name="Shubham"/> */}
-      <Navbar name={name}/>
-      hiii from react
+      <Navbar name={name} setter={setCount}/>
+      hiii from react {count}
+      {/* <button onClick={()=>{setCount(++count)}}>Incriment</button> */}
+      <button onClick={()=>{setCount((cnt)=>{
+        return cnt+1;
+      })}}>Incriment</button>
       <h1>Shubham</h1>
     </div>
   )
--- Sec-AF-AG/React/react-first-app/src/components/Navbar.jsx
+++ Sec-AF-AG/React/react-first-app/src/components/Navbar.jsx
@@ -1,11 +1,14 @@
 import "./Navbar.css"
 import { NavbarList } from "./NavbarList";
 
-const Navbar = ({name})=>{
+const Navbar = ({name,setter})=>{
 
   return (
     <h1 className="w-screen text-3xl bg-black text-white fixed top-0 p-5">
       This is Navbar {name}
+      <button onClick={()=>setter((count)=>{
+        return count+1;
+      })}>Click me</button>
       <NavbarList/>
     </h1>
   )
